c720534fe1020e406297ee4e9ac284ce916d0282
Collins Too <38856515+TooColline@users.noreply.github.com>
2021-06-20 19:20:38 +0300

    using prettier now

--- src/components/ContactDisplay.vue
+++ src/components/ContactDisplay.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="flex justify-center">
     <div class="flex bg-white rounded-[10px] px-[15px] py-[10px]">
-      <div class="flex items-center justify-center h-[50px] w-[50px] border border-blue-500 rounded-full mr-5">
+      <div
+        class="
+          flex
+          items-center
+          justify-center
+          h-[50px]
+          w-[50px]
+          border border-blue-500
+          rounded-full
+          mr-5
+        "
+      >
         <h1>{{ initials }}</h1>
       </div>
       <div class="flex flex-col">
-        <h1 class="font-bold"> {{ name }}</h1>
+        <h1 class="font-bold">{{ name }}</h1>
         <h3 class="text-gray-600">{{ myEmail }}</h3>
       </div>
     </div>
@@ -13,22 +24,20 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed } from "vue";
 
-const firstName = ref('Collins')
-const lastName = ref('Too')
-const myEmail = ref('collinstoo.ct@gmail.com')
+const firstName = ref("Collins");
+const lastName = ref("Too");
+const myEmail = ref("collinstoo.ct@gmail.com");
 
 const name = computed(() => {
-  return firstName.value + ' ' + lastName.value
-})
+  return firstName.value + " " + lastName.value;
+});
 
 const initials = computed(() => {
-  if(!firstName.value || !lastName.value) return ''
-  return firstName.value[0] + lastName.value[0]
-})
-
+  if (!firstName.value || !lastName.value) return "";
+  return firstName.value[0] + lastName.value[0];
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>

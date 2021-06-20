94262a68b6e26442d202cb844b8668c5fed33f40
Collins Too <38856515+TooColline@users.noreply.github.com>
2021-06-20 18:41:34 +0300

    setup a vue-playground progrect

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello {{ myName }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const myName = ref('Collins')
+
+</script>
+
+<style scoped>
+h1 {
+  @apply text-center;
+}
+</style>
